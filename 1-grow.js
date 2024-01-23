796b3a4a897f431e79dacf699b474cb4e4c1952f
mosyam0202@gmail.com <mosyam0202@gmail.com>
2024-01-23 13:20:58 +0700

    update description for 1/366

--- 1-grow.js
+++ 1-grow.js
@@ -1,9 +1,15 @@
-function countBy(x, n) {
-    var z = [];
-    for (let i = 1; i <= n ; i++){
-     z.push(x*i) 
-    }
-    
-    return z;
-  }
-  console.log(countBy(2, 5));
+// WPU Coding Challenge 2024
+// 1/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078
+
+// function grow(x) {
+//   let result = x[0];
+//   for (let i = 1; i < x.length; i++) {
+//     result *= x[i];
+//   }
+//   return result;
+// }
+
+const grow = (x) => x.reduce((acc, curr) => acc * curr);
+
+console.log(grow([2, 2, 2, 2, 2, 2]));
