1611a42520f83fff451f8f7e52fa1bc76231d95c
mosyam0202@gmail.com <mosyam0202@gmail.com>
2024-01-30 12:16:47 +0700

    update

--- 6-DNAtoRNA.js
+++ 6-DNAtoRNA.js
@@ -3,3 +3,26 @@
 // https://www.codewars.com/kata/5556282156230d0e5e000089
 
 
+// function DNAtoRNA(dna) {
+//     let rna ='';
+//     for (let i = 0; i < dna.length; i++){
+//         // if (dna[i] === 'T'){
+//         // rna += 'U';
+//         // }else{
+//         // rna += dna[i];
+//         // }
+//         rna += dna[i] === 'T' ? 'U' : dna[i];
+//     }
+//     return rna;
+//   }
+
+// const DNAtoRNA = (dna) => 
+//     dna
+//     .split('')
+//     .map((el) => 'T' ? 'U' : el)
+//     .join('')
+
+// const DNAtoRNA = (dna) => dna.replace(/T/g, 'U');
+const DNAtoRNA = (dna) => dna.replaceAll('T', 'U');
+
+  console.log(DNAtoRNA("TTTT"));
