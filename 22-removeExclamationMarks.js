e6b2052c3b4fbc9ce2ebce0f0d95ea8eef74076e
Mohammad Syam <mosyam0202@gmail.com>
2024-08-30 16:50:05 +0700

    22 / 366

---
+++ 22-removeExclamationMarks.js
@@ -0,0 +1,12 @@
+// function removeExclamationMarks(s) {
+//     let result = "";
+//     for(let i = 0; i < s.length; i++){
+//         if(s[i] !== "!")
+//         result += s[i];
+//     }
+//     return result;
+// }
+
+const removeExclamationMarks = (s) => s.split("!").join("");
+
+console.log(removeExclamationMarks("Hello World!"));
