bd807eaeedd84605232edccce88dee013fc86d51
Mohammad Syam <mosyam0202@gmail.com>
2024-09-03 13:07:16 +0700

    26 / 366

--- 26-updateLight.js
+++ 26-updateLight.js
@@ -1,14 +1,14 @@
-// function updateLight(current) {
-//   if(current === "green"){
-//     return "yellow"
-//   }  else if (current === "yellow") {
-//     return "red"
-//   } else {
-//     return "green"
-//   }
-// }
+function updateLight(current) {
+  if(current === "green"){
+    return "yellow"
+  }  else if (current === "yellow") {
+    return "red"
+  } else {
+    return "green"
+  }
+}
 
-const updateLight = (current) => current === "green" ? "yellow" : current === "yellow" ? "red" : "green";
+// const updateLight = (current) => current === "green" ? "yellow" : current === "yellow" ? "red" : "green";
 
 console.log(updateLight("green"));
 console.log(updateLight("yellow"));
