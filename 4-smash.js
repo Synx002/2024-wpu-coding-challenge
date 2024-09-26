e7099cff0f8d629de2bcb824410a9f261a0c3426
Mohammad Syam <mosyam0202@gmail.com>
2024-09-26 23:39:02 +0700

    4 / 366

--- 4-smash.js
+++ 4-smash.js
@@ -6,15 +6,15 @@
 //     return words.join(' ');
 // };
 
-const smash = (words) => words.join(' ');
+// const smash = (words) => words.join(' ');
 
-// function smash (words) {
-//     let result = ''
-//     for (i = 0; i < words.length; i++){
-//         result += words[i];
-//         if(i != words.length - 1) result += ' ';
-//     }
-//     return result
-//  }
+function smash (words) {
+    let result = ''
+    for (i = 0; i < words.length; i++){
+        result += words[i];
+        if(i != words.length - 1) result += ' ';
+    }
+    return result
+ }
 
 console.log(smash(['hello', 'world', 'this', 'is', 'great']));
