20d498abd76136814ece034f53e53e98f6b8eee5
Mohammad Syam <mosyam0202@gmail.com>
2024-09-26 23:54:12 +0700

    38 / 366

--- 38-getCount.js
+++ 38-getCount.js
@@ -1,12 +1,26 @@
+// function getCount(str) {
+//     let arr = ["a", "i", "u", "e", "o"];
+//     let counter = 0;
+//     for(let i = 0; i <= str.length; i++){
+//         if(arr.includes(str[i])){
+//             counter++
+//         }
+//     }
+//     return counter
+// }
+
 function getCount(str) {
-    let arr = ["a", "i", "u", "e", "o"];
-    let counter = 0;
-    for(let i = 0; i <= str.length; i++){
-        if(arr.includes(str[i])){
-            counter++
+    var vowelsCount = 0;
+    var vowels = ["a","e","i","o","u"];
+    for(var i = 0;i < str.length;i++){
+      for(var j=0;j<vowels.length;j++){
+        if(str[i] === vowels[j]){
+          vowelsCount++;
         }
+      }
     }
-    return counter
-}
+    
+    return vowelsCount;
+  }
 
 console.log(getCount("kontolodon"))
