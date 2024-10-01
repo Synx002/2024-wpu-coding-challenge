753378cfa409e15d19ad98377762c2fef15227c4
Mohammad Syam <mosyam0202@gmail.com>
2024-10-01 23:55:46 +0700

    39 / 366

--- 39-countWords.js
+++ 39-countWords.js
@@ -9,18 +9,28 @@
 //         return words
 // }
 
-function countWords(sentence) {
-    let words = 0;
+// function countWords(sentence) {
+//     let words = 0;
 
-    for (let i = 0; i < sentence.length; i++) {
-        // Jika karakter saat ini bukan spasi dan (karakter sebelumnya adalah spasi atau ini adalah karakter pertama)
-        if (sentence[i] !== " " && (i === 0 || sentence[i - 1] === " ")) {
-            words++;
+//     for (let i = 0; i < sentence.length; i++) {
+//         // Jika karakter saat ini bukan spasi dan (karakter sebelumnya adalah spasi atau ini adalah karakter pertama)
+//         if (sentence[i] !== " " && (i === 0 || sentence[i - 1] === " ")) {
+//             words++;
+//         }
+//     }
+
+//     return words;
+// }
+
+function countApla(str){
+    var result = 0;
+    for(let i = 0; i < str.length; i++){
+        if(str[i] !== " "){
+            result++;
         }
     }
-
-    return words;
+    return result;
 }
 
 
-console.log(countWords("Nama saya budiono siregar"))
+console.log(countApla("Nama saya budiono siregar"))
