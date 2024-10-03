b84f0babdaa2aec6a7907455184b886f839925ec
Mohammad Syam <mosyam0202@gmail.com>
2024-10-03 10:38:17 +0700

    42 / 366

---
+++ 42-evenOrOdd.js
@@ -0,0 +1,10 @@
+function evenOrOdd(number) {
+  if(number %2 === 0){
+    return "Even";
+  }
+    return "Odd";
+}
+
+console.log(evenOrOdd(2))
+console.log(evenOrOdd(7))
+console.log(evenOrOdd(-42))
