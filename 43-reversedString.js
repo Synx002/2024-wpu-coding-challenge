e6aaf2ef542d06ea792db5a2fa3da3c8811b5d5b
Mohammad Syam <mosyam0202@gmail.com>
2024-10-03 10:51:05 +0700

    43 / 366

---
+++ 43-reversedString.js
@@ -0,0 +1,10 @@
+// function solution(str){
+//   let result = str
+//   .split("")
+//   .reverse()
+//   .join("")
+//   return result;
+// }
+
+console.log(solution("world"));
+console.log(solution("hello"));
