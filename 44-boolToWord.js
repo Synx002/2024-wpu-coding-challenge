571776f5fee79a2c2ab79a9b376639db13f70c72
Mohammad Syam <mosyam0202@gmail.com>
2024-10-04 19:11:34 +0700

    44 / 366

--- 44-boolToWord.js
+++ 44-boolToWord.js
@@ -5,9 +5,11 @@
 //         return "No";
 // }
 
-function boolToWord(bool){
-    return bool === true ? "Yes" : "No";
-}
+// function boolToWord(bool){
+//     return bool === true ? "Yes" : "No";
+// }
+
+const boolToWord = (bool) => bool === true ? "Yes" : "No";
 
 console.log(boolToWord(true))
 console.log(boolToWord(false))
